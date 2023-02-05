192e9b70aeb2d4405c2399251b16869e009dbc1e
Anthony Margerand <anthony@lbcd.fr>
2023-02-05 20:24:32 +0100

    Add transparency patch

--- config.def.h
+++ config.def.h
@@ -112,6 +112,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8, alphaUnfocused = 0.6;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -152,6 +155,7 @@ unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
+unsigned int bg = 259, bgUnfocused = 259;
 
 /*
  * Default shape of cursor
@@ -193,36 +197,38 @@ static uint forcemousemod = ShiftMask;
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "font",         STRING,  &font },
-		{ "color0",       STRING,  &colorname[0] },
-		{ "color1",       STRING,  &colorname[1] },
-		{ "color2",       STRING,  &colorname[2] },
-		{ "color3",       STRING,  &colorname[3] },
-		{ "color4",       STRING,  &colorname[4] },
-		{ "color5",       STRING,  &colorname[5] },
-		{ "color6",       STRING,  &colorname[6] },
-		{ "color7",       STRING,  &colorname[7] },
-		{ "color8",       STRING,  &colorname[8] },
-		{ "color9",       STRING,  &colorname[9] },
-		{ "color10",      STRING,  &colorname[10] },
-		{ "color11",      STRING,  &colorname[11] },
-		{ "color12",      STRING,  &colorname[12] },
-		{ "color13",      STRING,  &colorname[13] },
-		{ "color14",      STRING,  &colorname[14] },
-		{ "color15",      STRING,  &colorname[15] },
-        { "background",   STRING,  &colorname[259] },
-        { "foreground",   STRING,  &colorname[258] },
-        { "cursorColor",  STRING,  &colorname[256] },
-		{ "termname",     STRING,  &termname },
-		{ "shell",        STRING,  &shell },
-		{ "minlatency",   INTEGER, &minlatency },
-		{ "maxlatency",   INTEGER, &maxlatency },
-		{ "blinktimeout", INTEGER, &blinktimeout },
-		{ "bellvolume",   INTEGER, &bellvolume },
-		{ "tabspaces",    INTEGER, &tabspaces },
-		{ "borderpx",     INTEGER, &borderpx },
-		{ "cwscale",      FLOAT,   &cwscale },
-		{ "chscale",      FLOAT,   &chscale },
+		{ "font",           STRING,  &font },
+		{ "color0",         STRING,  &colorname[0] },
+		{ "color1",         STRING,  &colorname[1] },
+		{ "color2",         STRING,  &colorname[2] },
+		{ "color3",         STRING,  &colorname[3] },
+		{ "color4",         STRING,  &colorname[4] },
+		{ "color5",         STRING,  &colorname[5] },
+		{ "color6",         STRING,  &colorname[6] },
+		{ "color7",         STRING,  &colorname[7] },
+		{ "color8",         STRING,  &colorname[8] },
+		{ "color9",         STRING,  &colorname[9] },
+		{ "color10",        STRING,  &colorname[10] },
+		{ "color11",        STRING,  &colorname[11] },
+		{ "color12",        STRING,  &colorname[12] },
+		{ "color13",        STRING,  &colorname[13] },
+		{ "color14",        STRING,  &colorname[14] },
+		{ "color15",        STRING,  &colorname[15] },
+        { "background",     STRING,  &colorname[259] },
+        { "foreground",     STRING,  &colorname[258] },
+        { "cursorColor",    STRING,  &colorname[256] },
+		{ "termname",       STRING,  &termname },
+		{ "shell",          STRING,  &shell },
+		{ "minlatency",     INTEGER, &minlatency },
+		{ "maxlatency",     INTEGER, &maxlatency },
+		{ "blinktimeout",   INTEGER, &blinktimeout },
+		{ "bellvolume",     INTEGER, &bellvolume },
+		{ "tabspaces",      INTEGER, &tabspaces },
+		{ "borderpx",       INTEGER, &borderpx },
+		{ "cwscale",        FLOAT,   &cwscale },
+		{ "chscale",        FLOAT,   &chscale },
+        { "alpha",          FLOAT,   &alpha },
+        { "alphaUnfocused", FLOAT,   &alphaUnfocused },
 };
 
 /*
--- st.h
+++ st.h
@@ -87,6 +87,7 @@ typedef union {
 
 void die(const char *, ...);
 void redraw(void);
+void tfulldirt(void);
 void draw(void);
 
 void externalpipe(const Arg *);
@@ -144,3 +145,4 @@ extern unsigned int defaultfg;
 extern unsigned int defaultbg;
 extern unsigned int defaultcs;
 extern const int boxdraw, boxdraw_bold, boxdraw_braille;
+extern float alpha, alphaUnfocused;
